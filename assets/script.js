3bf4ec4cd9f29711895d02d17d078379cdb15ba8
ValKer-OPCS <icyfry@gmail.com>
2025-08-28 09:40:44 +0200

    refactor : dot generation is now a function
    turned dot generation into a function and called that function after banner update

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
@@ -26,20 +26,8 @@ const bannerImg = document.querySelector(".banner-img");
 const bannerText = document.querySelector("#banner p");
 const dotsContainer = document.querySelector(".dots");
 
+//Fonctions
 
-flecheGauche.addEventListener("click", () => changeSlide(-1));
-flecheDroite.addEventListener("click", () => changeSlide(1));
-
-	slides.forEach((_, i) => {
-		const dot = document.createElement("span");
-		dot.classList.add("dot");
-		if (i === 0) dot.classList.add("dot_selected");
-		dotsContainer.appendChild(dot);
-	});
-
-updateBanner();
-
-//////////////////////////////////
 function updateBanner() {
 	const slide = slides[index];
 	bannerImg.src = `./assets/images/slideshow/${slide.image}`;
@@ -51,10 +39,30 @@ function updateBanner() {
 }
 
 function changeSlide(direction) {
-    index += direction;
-    if (index < 0) index = slides.length - 1;
-    if (index >= slides.length) index = 0;
-    updateBanner();
+	index += direction;
+	if (index < 0) index = slides.length - 1;
+	if (index >= slides.length) index = 0;
+	updateBanner();
 }
+
+function genererDots(direction) {
+	slides.forEach((_, i) => {
+	const dot = document.createElement("span");
+	dot.classList.add("dot");
+	if (i === 0) dot.classList.add("dot_selected");
+	dotsContainer.appendChild(dot);
+});
+}
+
+// Listeners pour les flèches
+
+flecheGauche.addEventListener("click", () => changeSlide(-1));
+flecheDroite.addEventListener("click", () => changeSlide(1));
+
+
+// Initialisation de la bannière
+updateBanner();
+genererDots();
+
 
 
